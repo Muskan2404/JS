f5cb36cbaef178d59aa90ed81d5e1fba0e285d49
Muskan2404 <144544617+Muskan2404@users.noreply.github.com>
2024-07-01 15:07:38 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,42 @@
+let score = "44";
+//let score = "44abc";
+//let score = null;
+//let score = undefined;
+//let score = true;
+
+/*
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+*/
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+
+/*
+//let isLoggedIn =  1;
+let isLoggedIn = 12;
+
+let booleanIsLoggedIn =  Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+*/
+
+// 1 => true; 0 => false; every other number => true
+// undefined => false
+// null => false
+// "" => false
+// ; => false
+// "abse" => true 
+
+let someNumber = "abs34";
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log( typeof stringNumber);
