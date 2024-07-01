ba139ff8078708bccc22d3d00be9564a086de6ab
Muskan2404 <144544617+Muskan2404@users.noreply.github.com>
2024-07-01 14:34:11 +0000

    third

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("error"); // we are using nodejs, not browser
+
+let name = "Muskan";
+let age = 19;
+let isLoggedIn = true;
+
+
+// number  => 2 to power of 53
+// bigint
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+// object
+
+console.log(typeof "muskan");
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
