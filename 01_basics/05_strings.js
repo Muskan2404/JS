09cb51daede1cb50615e9c4a1eb2afb76924668c
Muskan2404 <144544617+Muskan2404@users.noreply.github.com>
2024-07-02 16:13:43 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,41 @@
+const name = "muskan";
+const repoCount = 20;
+
+//console.log(name + repoCount + " value");  //not recommended syntax
+
+console.log(`Hello my name is ${name}. I have  ${repoCount} repositories.`);
+
+const gameName = new String('obscure robot');
+
+console.log(typeof gameName);
+
+console.log(gameName [7]);
+console.log(gameName.__proto__);
+
+ console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.toLocaleUpperCase());
+ console.log(gameName.charAt(12));
+// console.log(gameName.indexOf('b'));
+
+
+const newString = gameName.substring(0,6);
+console.log(newString);
+
+const anotherString = gameName.slice(-13, 4);
+console.log(anotherString);
+
+
+const newStringOne = "     muskan      ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+const url = "https://hogwarts.com/hogwarts%20world";
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('muskan'));
+
+console.log(gameName.split(' ')); 
