3a41a95457e69dbcfa8826e1f7708b46a0aab1a4
Muskan2404 <144544617+Muskan2404@users.noreply.github.com>
2024-07-03 16:48:02 +0000

    uncomment

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -14,9 +14,9 @@ console.log(gameName.__proto__);
 
  console.log(gameName.length);
 // console.log(gameName.toUpperCase());
-// console.log(gameName.toLowerCase());
+ console.log(gameName.toLowerCase());
 // console.log(gameName.toLocaleUpperCase());
- console.log(gameName.charAt(12));
+ //console.log(gameName.charAt(2));
 // console.log(gameName.indexOf('b'));
 
 
